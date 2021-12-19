47616f6ae9d91ea327fac5d10f7d740a84cf1a80
iradukunda irenee <iradukundairenee9@gmail.com>
2021-12-19 12:28:41 +0200

    kabaye

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fullstack Authentication Example with Next.js and NextAuth.js
+
+This is the starter project for the fullstack tutorial with Next.js and Prisma.
---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react"
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+      <SessionProvider session={session}>
+            <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
